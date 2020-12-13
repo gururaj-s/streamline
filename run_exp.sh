74307dee232dfd10bd150a0c7c48e1a8ab3f08dc
Gururaj Saileshwar <gsaileshwar3@gatech.edu>
2020-12-12 22:22:26 -0500

    Add run_exp.sh

---
+++ run_exp.sh
@@ -0,0 +1,15 @@
+#Run the base attack (Fig-9, Table-2)
+echo "Running base attack (Fig-9, Table-2)"
+cd results/base; ./run_base.sh
+
+#Run the attack with ECC (Table-3)
+echo "Running attack with ECC (Table-3)"
+cd ../../results/ecc; ./run_ecc.sh
+
+#Run sensitivity to varying shared array sizes (Table-3)
+echo "Running sensitivity to varying array sizes (Table-4)"
+cd ../../results/array_sz; ./run_array_sz.sh
+
+#Run sensitivity to varying synchronization periods (Table-4)
+echo "Running sensitivity to varying synchronization periods (Table-4)"
+cd ../../results/sync_period; ./run_sync_period.sh
