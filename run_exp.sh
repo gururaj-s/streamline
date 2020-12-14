69e8b0496525a25bd7ab4047939909888533f0f1
Gururaj Saileshwar <gsaileshwar3@gatech.edu>
2020-12-14 12:48:06 -0500

    Added spaces to run_exp.sh

--- run_exp.sh
+++ run_exp.sh
@@ -3,13 +3,16 @@ echo "Running base attack (Fig-9, Table-2)"
 cd results/base; ./run_base.sh
 
 #Run the attack with ECC (Table-3)
+echo " "
 echo "Running attack with ECC (Table-3)"
 cd ../../results/ecc; ./run_ecc.sh
 
 #Run sensitivity to varying shared array sizes (Table-3)
+echo " "
 echo "Running sensitivity to varying array sizes (Table-4)"
 cd ../../results/array_sz; ./run_array_sz.sh
 
 #Run sensitivity to varying synchronization periods (Table-4)
+echo " "
 echo "Running sensitivity to varying synchronization periods (Table-4)"
 cd ../../results/sync_period; ./run_sync_period.sh
